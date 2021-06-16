28046446cbf80bfe6319f762b7c2f28c00c70dcc
Jorge9501 <84034571+Jorge9501@users.noreply.github.com>
2021-06-16 14:34:24 -0500

    Update DatosModel.php

--- codeingniter/app/Models/DatosModel.php
+++ codeingniter/app/Models/DatosModel.php
@@ -1,1 +1,49 @@
+<?php
+namespace App\Models;
+use CodeIgnater\Model;
+class DatosModel extends Model{
+    public fuction obtenerInformacion($data){
+           $gModel =$this->$this->db->table('persona'):
+           $Model->whare($data);
+           return $gModel->get()->getResultArray();
+      
+      
+    }
+  
+  
+    public fuction listarTodo(){
+         $gModel = $this ->db->query("SELECT * FROM persona");
+          return $gModel->getResult();
+      
+      
+    }
+  
+    
+    public fuction insertar($data){
+          $gModel = $this->db->table('persona');
+          $gModel->insert($data);
+          return $this->db->insertID();
+      
+     
+    }
+  
+  public fuction actualizar($data,$id){
+        $gModel = $this->db->table('persona');
+        $gModel->set($data);
+        $gModel->where($id);
+        return $gModel->update();
+    
+    
+  }
+  
+  public fuction eliminar($id){
+        $gModel = $this->db->table('persona');
+        $gModel->where($id);
+        return $gModel->delete();
+    
+  }
+  
+ 
+}
+    
 
